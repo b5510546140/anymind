32d70c0813b77702804782ca27d144fcbb450ac3
wat wattanagaroon <>
2022-01-23 22:26:07 +0700

    Handle code when some of input is before the newest data. Then re calculate all data.

--- src/main/java/com/anyMind/application/wallet/WalletController.java
+++ src/main/java/com/anyMind/application/wallet/WalletController.java
@@ -1,5 +1,6 @@
 package com.anyMind.application.wallet;
 
+import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,27 +28,67 @@ public class WalletController {
         int i =0;
         int maxSize = depositHistory.size()-1;
         Wallet w = depositHistory.get(i);
+        Wallet tempWallet = new Wallet();
+        tempWallet.setBalance(0.0);
         while(endOfHourTimeFrom.compareTo(endOfHourTimeTo)<= 0) {
             if (w.getDatetime().compareTo(endOfHourTimeFrom)< 0 ) {
                 // This is the last index of array
                 if (i != maxSize){
+                    tempWallet.setBalance(w.getBalance());
                     w = depositHistory.get(++i);
+                } else {
+                    // last data
+                    tempWallet.setBalance(w.getBalance());
                 }
             }
-            walletResult.add(new WalletResponce(endOfHourTimeFrom,w.getBalance()));
+            walletResult.add(new WalletResponce(endOfHourTimeFrom,tempWallet.getBalance()));
             endOfHourTimeFrom = endOfHourTimeFrom.plusHours(1);
         }
         return walletResult;
     }
 
     @PostMapping("/deposit")
-    Wallet deposit(@RequestBody Wallet newDepoit) {
+    Wallet deposit(@RequestBody Wallet newDeposit) {
         Wallet wallet = walletRepository.findTopByOrderByAccountIdDesc();
-        if (wallet.getDatetime().compareTo(newDepoit.getDatetime()) > 0 ){
-    // newDepoit input is occur before last data
+        // newDepoit input is occur before last data
+        Wallet temp;
+        if (wallet.getDatetime().compareTo(newDeposit.getDatetime()) > 0 ){
+
+            List<Wallet> depositHistory =  walletRepository.findBydateBetween(newDeposit.getDatetime(),wallet.getDatetime());
+            double prevAmount = newDeposit.getAmount();
+            double prevBalance = 0.0;
+            ZonedDateTime prevDatetime = newDeposit.getDatetime();
+            if (depositHistory.size() > 0) {
+                prevBalance = depositHistory.get(0).getBalance() - depositHistory.get(0).getAmount();
+                double tempAmount;
+                ZonedDateTime tempDatetime ;
+                for (Wallet w : depositHistory) {
+                    tempAmount = w.getAmount();
+                    tempDatetime = w.getDatetime();
+
+                    w.setAmount(prevAmount);
+                    w.setDatetime(prevDatetime);
+
+                    w.setBalance(prevBalance + prevAmount);
+                    walletRepository.save(w);
+
+                    prevBalance = w.getBalance();
+                    prevAmount = tempAmount;
+                    prevDatetime = tempDatetime;
+                }
+            }
+            Wallet lastToAdd = new Wallet();
+            lastToAdd.setAmount(prevAmount);
+            lastToAdd.setDatetime(prevDatetime);
+
+            lastToAdd.setBalance(prevBalance + prevAmount);
+            temp = walletRepository.save(lastToAdd);
         }
-        newDepoit.setBalance(wallet.getBalance()+newDepoit.getAmount());
-        return walletRepository.save(newDepoit);
+        else {
+            newDeposit.setBalance(wallet.getBalance() + newDeposit.getAmount());
+            temp = walletRepository.save(newDeposit);
+        }
+        return temp;
     }
 
 
